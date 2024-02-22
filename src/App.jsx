2ac9ac969caa34974dd85a048ab499c5df8fa8d3
Abhishek Verma <abhishekverma6533@gmail.com>
2024-02-22 18:28:06 +0530

    start creating startGame page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import styled from 'styled-components';
+import StartGame from './components/StartGame';
+
+const App = () => {
+  return (
+    <div>
+       <StartGame/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/components/StartGame.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StartGame = () => {
+  return (
+    <div>
+      Hi this is StartGame Component
+    </div>
+  )
+}
+
+export default StartGame
